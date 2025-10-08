95f079b65694fc4eb1deb0f222a7ddb2cab38830
Krasnopolskiy Ilya <162899632+vomas7@users.noreply.github.com>
2025-10-08 11:28:01 +0300

    Update qad_entity.py
    fixed the layer type check using isinstance

--- qad_entity.py
+++ qad_entity.py
@@ -78,7 +78,7 @@ class QadEntity():
    # ===============================================================================
    def set(self, layer_or_entity, featureId = None):
       self.clear()
-      if type(layer_or_entity) == QgsVectorLayer:
+      if isinstance(layer_or_entity, QgsVectorLayer):
          self.layer = layer_or_entity # il layer non si può copiare
          self.featureId = featureId # copio l'identificativo di feature
       else: # layer è una entità
@@ -488,7 +488,7 @@ class QadLayerEntitySet():
       return self.layer.crs()
 
    def set(self, layer, features = None):
-      if type(layer) == QgsVectorLayer:
+      if isinstance(layer, QgsVectorLayer):
          self.layer = layer # il layer non si può copiare
          self.featureIds = []       
          if features is not None:
@@ -802,7 +802,7 @@ class QadEntitySet():
    def findLayerEntitySet(self, layer):
       if layer is None:
          return None
-      if type(layer) == QgsVectorLayer: # layer
+      if isinstance(layer, QgsVectorLayer): # layer
          return self.findLayerEntitySet(layer.id())     
       elif type(layer) == unicode: # id del layer
          for layerEntitySet in self.layerEntitySetList:
@@ -1174,3 +1174,4 @@ def getSelSet(mode, mQgsMapTool, points = None, \
             
    #QApplication.restoreOverrideCursor()
    return result
+
